5bf377190453950d21b5a01c81df312022ff6e09
china_wangyu <china_wangyu@aliyun.com>
2019-12-23 17:05:58 +0800

    更新配置文件

--- src/paa.php
+++ src/paa.php
@@ -1,5 +1,18 @@
 <?php
 /** Created by 嗝嗝<china_wangyu@aliyun.com>. Date: 2019-12-17  */
 return [
-    'jwt' => \config('lincms.jwt')
+    'jwt' => \config('lincms.jwt'),
+    "management"=> [ # 接口管理平台
+        'enable' => true, # 开关控制，true：开启｜false：关闭
+        'member' => [
+            'admin' => [
+                'password' => 'supper',
+                'supper' => true, # true：超级管理员｜false：浏览者
+            ],
+            'web' => [
+                'password' => '123456',
+                'supper' => false, # true：超级管理员｜false：浏览者
+            ]
+        ],
+    ]
 ];
