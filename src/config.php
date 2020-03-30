84a34dd57c53fd9fddc8c7c1abb8a2d71c2143a4
china_wangyu <china_wangyu@aliyun.com>
2020-03-30 16:23:18 +0800

    '新建dev分支,更新依赖版本'

--- src/config.php
+++ src/config.php
@@ -11,11 +11,15 @@ return [
     ],
     'ignore' => [],
     'custom' => [
+        'enable'      => true,
         # 格式：注解类 => 注解操作类
-        \paa\annotation\Param::class => \paa\annotation\handler\Param::class, # 单个参数验证器
-        \paa\annotation\Jwt::class => \paa\annotation\handler\Jwt::class, # JWT注解验证器
-        \paa\annotation\Doc::class => \paa\annotation\handler\Doc::class, # 文档管理器
-        \LinCmsTp6\annotation\Auth::class => \LinCmsTp6\annotation\handler\Auth::class, # lin-cms 权限控制器
-        \LinCmsTp6\annotation\Logger::class => \LinCmsTp6\annotation\handler\Logger::class, # lin-cms 日志监控器
-    ]
+        'namespaces' => [
+            # 格式：注解类 => 注解操作类
+            \paa\annotation\Param::class => \paa\annotation\handler\Param::class, # 单个参数验证器
+            \paa\annotation\Jwt::class => \paa\annotation\handler\Jwt::class, # JWT注解验证器
+            \paa\annotation\Doc::class => \paa\annotation\handler\Doc::class, # 文档管理器
+            \LinCmsTp6\annotation\Auth::class => \LinCmsTp6\annotation\handler\Auth::class, # lin-cms 权限控制器
+            \LinCmsTp6\annotation\Logger::class => \LinCmsTp6\annotation\handler\Logger::class, # lin-cms 日志监控器
+        ],
+    ],
 ];
