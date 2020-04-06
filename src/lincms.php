c71fc6f02fa10f4b43a61c883eaf2f2a03101619
china_wangyu <china_wangyu@aliyun.com>
2020-04-06 16:41:10 +0800

    '修复JWT鉴权和刷新鉴权的问题'

--- src/lincms.php
+++ src/lincms.php
@@ -7,7 +7,8 @@ return [
         'type' => 'Bearer', // 授权类型
         'request' => 'header', // 请求方式
         'param' => 'authorization', // 授权名称
-        'time' => 7200, //token有效时长
+        'access_exp' => 7200, //access_token 过期时间：2小时
+        'refresh_exp' => 84000 * 30, //refresh_token 过期时间：30天
         'payload' => [
             'iss' => 'PAA-ThinkPHP6', //签发者
             'iat' => '', //什么时候签发的
@@ -15,6 +16,19 @@ return [
             'uniqueId' => '',
             'signature' => ''
         ]
+    ],
+    "management"=> [ # 接口管理平台
+        'enable' => true, # 开关控制，true：开启｜false：关闭
+        'member' => [
+            'admin' => [
+                'password' => 'supper',
+                'admin' => true, # true：超级管理员｜false：浏览者
+            ],
+            'web' => [
+                'password' => '123456',
+                'admin' => false, # true：超级管理员｜false：浏览者
+            ]
+        ],
     ],
     'file' => [
         'image_type' => ['jpg','jpeg','png','gif'],
